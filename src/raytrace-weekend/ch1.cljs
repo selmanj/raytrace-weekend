cedc12d3f13b7fcb84ab43fb918c97de14441321
Joe Selman <selman.joe@gmail.com>
2017-06-28 22:15:56 -0700

    Cleaned up rendering code a bit

--- src/raytrace-weekend/ch1.cljs
+++ src/raytrace-weekend/ch1.cljs
@@ -2,20 +2,22 @@
   (:require [reagent.core :as r]
             [goog.object :as object]))
 
-(defn canvas-component []
-  (let [dom-node (r/atom nil)]
-    (r/create-class
-     {:component-did-mount
-      (fn [this]
-        (reset! dom-node (r/dom-node this))
-        ;; Temporary renders for now
-        (let [ctx (.getContext @dom-node "2d")]
-          (object/set ctx "fillStyle" "green")
-          (.fillRect ctx 10 10 100 100)))
+(defn canvas-render [dom-node]
+  (let [ctx (.getContext dom-node "2d")]
+    (object/set ctx "fillStyle" "green")
+    (.fillRect ctx 10 10 100 100)))
 
-      :reagent-render
-      (fn []
-        [:canvas#ch1-canvas {:width 400 :height 200 :style {:border "1px solid #000000"}}])})))
+(defn canvas-component []
+  (r/create-class
+   {:component-did-mount
+    (fn [this] (canvas-render (r/dom-node this)))
+
+    :reagent-render
+    (fn []
+      [:canvas#ch1-canvas {:width 400
+                           :height 200
+                           :style {:border "1px solid #000000"}}])}))
 
 (defn ^:export mount []
-  (r/render [canvas-component] (js/document.getElementById "ch1-app")))
+  (r/render [canvas-component]
+            (js/document.getElementById "ch1-app")))
