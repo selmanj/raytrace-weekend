e9996095ab418a9f0d16a9bb41d1af643f173b65
Joe Selman <selman.joe@gmail.com>
2017-06-28 23:10:40 -0700

    Add cljs devtools for debugging

--- project.clj
+++ project.clj
@@ -1,13 +1,15 @@
 (defproject raytrace-weekend "0.0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/clojurescript "1.9.660"]
+                 [binaryage/devtools "0.9.4"]
                  [reagent "0.7.0"]]
   :plugins [[lein-figwheel "0.5.10"]]
   :clean-targets ^{:protect false} [:target-path "out" "resources/public/cljs"]
   :cljsbuild {:builds [{:id "dev"
                         :source-paths ["src"]
                         :figwheel {:on-jsload "raytrace-weekend.ch1/mount"}
-                        :compiler {:main "raytrace-weekend.core"
+                        :compiler {:preloads [devtools.preload]
+                                   :main "raytrace-weekend.core"
                                    :asset-path "cljs/out"
                                    :output-to "resources/public/cljs/raytrace-weekend.js"
                                    :output-dir "resources/public/cljs/out"}}]})
