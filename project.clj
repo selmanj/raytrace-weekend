e5a35bb64f5ae419245f9410062a63c23b774854
Joe Selman <selman.joe@gmail.com>
2017-06-28 17:42:21 -0700

    Move index/cljs to subdirs

--- project.clj
+++ project.clj
@@ -2,8 +2,11 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/clojurescript "1.9.660"]]
   :plugins [[lein-figwheel "0.5.10"]]
-  :clean-targets [:target-path "out"]
+  :clean-targets ^{:protect false} [:target-path "out" "resources/public/cljs"]
   :cljsbuild {:builds [{:id "dev"
                         :source-paths ["src"]
                         :figwheel true
-                        :compiler {:main "raytrace-weekend.core"}}]})
+                        :compiler {:main "raytrace-weekend.core"
+                                   :asset-path "cljs/out"
+                                   :output-to "resources/public/cljs/raytrace-weekend.js"
+                                   :output-dir "resources/public/cljs/out"}}]})
